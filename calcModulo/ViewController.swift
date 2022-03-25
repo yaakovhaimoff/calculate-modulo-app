c3cb73d7c87b1fc197640388653ea852ba127e71
Yaakov Haimoff <hyaakov@aol.com>
2022-03-26 02:57:49 +0300

    Initial Commit

---
+++ calcModulo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calcModulo
+//
+//  Created by Yaakov Haimoff on 26/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
